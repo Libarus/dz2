4c4a9f6ab4075ff309a4eb83ec9f71adc1c7a4cb
Anton Zabelin <libarus@gmail.com>
2016-02-28 17:58:03 +0300

    Промежуточный релиз. Свёрстан 1 вид

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
     <li><strong>Установка:</strong></li>
     <li>git clone https://github.com/Libarus/dz2.git</li>
     <li>npm i</li>
+    <li>bower i</li>
+    <li><strong>gulp</strong> или <strong>gulp watch</strong> для сборки проекта</li>
 </ol>
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    spritesmith = require('gulp.spritesmith'),
+    notify = require("gulp-notify");
 
 var paths = {
     src: {
         dev:'dev/**/*.*',
         jade:'dev/**/*.jade',
-        scss:'dev/scss/**/*.scss'
+        scss:'dev/scss/**/*.scss',
+        sprites: 'dev/sprites/*.png'
     },
     dest:{
         app:'app/',
         jade:'app/',
-        scss:'app/css/'
+        scss:'app/css/',
+        sprites: 'app/images/'
     }
 };
 
@@ -26,13 +30,34 @@ gulp.task('jade',function () {
 
     gulp.src(paths.src.jade)
         .pipe(jade({
-            locals: YOUR_LOCALS,
-            pretty: '    '
+             locals: YOUR_LOCALS
+            //,pretty: '    '
         }))
         .pipe(gulp.dest(paths.dest.jade));
 });
 
-gulp.task('default',['jade','sass']);
+gulp.task('sprite', function () {
+    var spriteData = gulp.src(paths.src.sprites)
+        .pipe(spritesmith({
+                imgName: '../images/sprite.png',
+                cssName: 'sprite.css',
+                algorithm: "top-down"
+        }));
+    spriteData.img.pipe(gulp.dest(paths.dest.sprites));
+    spriteData.css.pipe(gulp.dest(paths.dest.scss));
+});
+
+gulp.task('notify',function () {
+    gulp.src(paths.src.dev)
+        .pipe(through(function () {
+            this.emit("error", new Error("Something happend: Error message!"))
+        }))
+        .on("error", notify.onError(function (error) {
+            return "Message to the notifier: " + error.message;
+        }));
+})
+
+gulp.task('default',['jade','sass','sprite']);
 
 gulp.task('watch',function () {
     gulp.watch(paths.src.dev,['default']);
